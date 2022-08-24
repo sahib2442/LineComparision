2bec5e00f535349c2d593bd72c8f2cd5419b9fe5
sahib2442 <kadersahib1234@gmail.com>
2022-08-24 14:46:06 +0530

    [sahib] Add: two line comparision

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -5,8 +5,19 @@ namespace LineComparision
     {
         static void main(string[]args)
         {
-            LineComparision linecomparision = new LineComparision(1, 2, 3, 4);
-            linecomparision.CalculateLength();
+            LineComparision lineComProblem = new LineComparision(1,2,3,4)
+            double line1 = lineComProblem.CalculateLength();
+            Console.WriteLine("length of the first line" + line1);
+            LineCompProblem lineCompProblem2 = new LineCompProblem(1, 2, 3, 4);
+            double line2 = lineComProblem.CalculateLength();
+            if (line1 == line2)
+            {
+                Console.WriteLine("line1 is equal to line2");
+            }
+            else
+            {
+                Console.WriteLine("line1 is not equal to line2");
+            }
         }
     }
 }
